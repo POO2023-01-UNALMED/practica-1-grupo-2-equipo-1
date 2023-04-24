dc3b5bb2111e4b18a69beb8cbc045d409d6d2239
alejandro20019 <aferiag@unal.edu.co>
2023-04-24 13:14:40 -0500

    base Hotel

--- Hotel/src/gestorAplicacion/hoteles/Hotel.java
+++ Hotel/src/gestorAplicacion/hoteles/Hotel.java
@@ -5,4 +5,20 @@ import java.util.*;
 
 public class Hotel {
     private List<Habitacion> habitaciones = new ArrayList<>();
+
+    public Hotel(){
+        
+    }
+
+    public void agragarHabitacion(Habitacion habitacion){
+        habitaciones.add(habitacion);
+    }
+
+    public String mostrarHabitaciones(){
+        String s = "";
+        for (Habitacion habitacion : habitaciones) {
+            s += habitacion.toString() + System.lineSeparator();
+        }
+        return s;
+    }
 }
