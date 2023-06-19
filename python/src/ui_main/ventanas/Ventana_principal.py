7c1a0cd0784caa6301753ea2384550b7c90aa341
alejandro20019 <aferiag@unal.edu.co>
2023-06-18 22:56:12 -0500

    error ortografico

--- python/src/ui_main/ventanas/Ventana_principal.py
+++ python/src/ui_main/ventanas/Ventana_principal.py
@@ -12,19 +12,19 @@ def inicializar():
     ventana.config(menu=menu_inicio, height=30)
 
     Archivo = tk.Menu(menu_inicio)
-    proseso_comsulta = tk.Menu(menu_inicio)
+    proceso_consulta = tk.Menu(menu_inicio)
     ayuda = tk.Menu(menu_inicio)
 
     menu_inicio.add_cascade(menu=Archivo, label="Archivo")
     Archivo.add_command(label="Aplicación") #Terminar
     Archivo.add_command(label="Salir", command=salir) #terminar
 
-    menu_inicio.add_cascade(menu=proseso_comsulta, label="Procesos y consultas")
-    proseso_comsulta.add_command(label="Alojar Huésped")
-    proseso_comsulta.add_command(label="Desalojar Huésped/Factura")
-    proseso_comsulta.add_command(label="Restaurante y alimentación")
-    proseso_comsulta.add_command(label="Transporte")
-    proseso_comsulta.add_command(label="Servicios extra")
+    menu_inicio.add_cascade(menu=proceso_consulta, label="Procesos y consultas")
+    proceso_consulta.add_command(label="Alojar Huésped")
+    proceso_consulta.add_command(label="Desalojar Huésped/Factura")
+    proceso_consulta.add_command(label="Restaurante y alimentación")
+    proceso_consulta.add_command(label="Transporte")
+    proceso_consulta.add_command(label="Servicios extra")
     
     menu_inicio.add_cascade(menu=ayuda, label="Ayuda")
     ayuda.add_command(label="Acerca de")
