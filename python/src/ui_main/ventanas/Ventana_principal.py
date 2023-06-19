a077c101a741c76a91bad84d2caee46f22af4391
alejandro20019 <aferiag@unal.edu.co>
2023-06-18 23:08:30 -0500

    Update Ventana_principal.py

--- python/src/ui_main/ventanas/Ventana_principal.py
+++ python/src/ui_main/ventanas/Ventana_principal.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import *
 from ui_main.ventanas.ventana_base import ventana
 
-frame_principal = Frame(ventana, bg="green", width=400, height=400)
+frame_principal = Frame(ventana, bg="green", width=1090, height=670)
 frame_principal.pack_propagate(False)
 frame_principal.grid_propagate(False)
 
