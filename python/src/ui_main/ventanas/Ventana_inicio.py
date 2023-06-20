62e912866b1df5dcf62176e0831a81abae2f14ef
jmarquezba <124936850+jmarquezba@users.noreply.github.com>
2023-06-19 23:14:23 -0500

    Update Ventana_inicio.py

--- python/src/ui_main/ventanas/Ventana_inicio.py
+++ python/src/ui_main/ventanas/Ventana_inicio.py
@@ -8,35 +8,44 @@ from base_datos.Serializador import serializar
 from ui_main.main import hotel
 
 
+
 frame_inicio = Frame(ventana)
 frame_inicio.pack()
+
 
 p1Frame=Frame(frame_inicio,width=530, height=650,background="grey")
 p1Frame.grid(row=0,column=0,padx=5,pady=5)
 p1Frame.grid_propagate(False)
 
+
 p2Frame=Frame(frame_inicio,width=530, height=650,bd=5, relief="solid", highlightbackground="red")
 p2Frame.grid(row=0,column=1,padx=5,pady=5)
 p2Frame.grid_propagate(False)
+
 
 p3Frame=Frame(p1Frame,width=520, height=100,bd=5, relief="solid", highlightbackground="red")
 p3Frame.grid(row=0,column=0,padx=5,pady=5)
 p3Frame.grid_propagate(False)
 
+
 p4Frame=Frame(p1Frame,width=520, height=535,bg="white")
 p4Frame.grid(row=1,column=0,padx=5,pady=0)
 p4Frame.grid_propagate(False)
+
 
 p5Frame=Frame(p2Frame,width=510, height=100,bg="white")
 p5Frame.grid(row=0,column=0,padx=5,pady=5)
 p5Frame.grid_propagate(False)
 
+
 p6Frame=Frame(p2Frame,width=510, height=525,bg="white")
 p6Frame.grid(row=1,column=0,padx=5,pady=0)
 p6Frame.grid_propagate(False)
 
+
 #Saludo de bienvenida
 saludo = "Bienvenido al sistema de gestion del hotel The Debugg Inn"
+
 
 label_bienvenida = Label(p3Frame, text=saludo, font=("Arial",12), fg="black")
 label_bienvenida.place(relx=0.5, rely=0.3, anchor="s")#esta centrado
@@ -54,6 +63,7 @@ label_descripcion.place(x=0, y=0)
 
 def mostrar_ocultar_Descripcion():
     label_descripcion.config(text=next(descripcion))
+
 
 #Imagene3
 # Imágenes
@@ -106,8 +116,10 @@ dict_imgs = {
     )
 }
 
+
 def evento_hotel(evento):
     cinco.config(image=next(dict_imgs["Hotel"]))
+
 
 def eveto_label(evento):
     # cambio nombre
@@ -118,29 +130,37 @@ def eveto_label(evento):
     tres.config(image=next(dict_imgs["abajo_izquierda"]))
     cuatro.config(image=next(dict_imgs["abajo_derecha"]))
 
+
 # Cambiar imágenes
+
 
 uno = Label(p6Frame, width=250, height=250, bg="white", image=next(dict_imgs["arriba_izquierda"]), bd=1, relief="solid")
 uno.grid(row=0, column=0, padx=0, pady=5)
 
+
 dos = Label(p6Frame, width=250, height=250, bg="white", image=next(dict_imgs["arriba_derecha"]), bd=1, relief="solid")
 dos.grid(row=0, column=1, padx=0, pady=5)
+
 
 tres = Label(p6Frame, width=250, height=250, bg="white", image=next(dict_imgs["abajo_izquierda"]), bd=1, relief="solid")
 tres.grid(row=1, column=0, padx=0, pady=5)
 
+
 cuatro = Label(p6Frame, width=250, height=250, bg="white", image=next(dict_imgs["abajo_derecha"]), bd=1, relief="solid")
 cuatro.grid(row=1, column=1, padx=0, pady=5)
+
 
 cinco = Label(p4Frame, width=500, height=400, bg="white", image=next(dict_imgs["Hotel"]), bd=2, relief="solid")
 cinco.place(relx=0.5, rely=0.9,anchor="s")
 cinco.bind("<Enter>", evento_hotel) 
 
 
+
 #Breve hoja de vida de cada desarrollador
 #derecha superior (p5)
 
 #Cuando llega al ultimo se devuelve al inicio
+
 
 circulo = cycle(("Alejandro Feria\nHoja de Vida\n\nFecha de Nacimiento:20/07/2004\nGustos: Programar y tocar musica <3"
                  , "Juan Miguel Márquez\nHoja de Vida\n\nFecha de Nacimiento:02/05/2004\nGustos: Ir al gimnasio, escuchar música y cocinar :)"
@@ -151,9 +171,11 @@ circulo = cycle(("Alejandro Feria\nHoja de Vida\n\nFecha de Nacimiento:20/07/200
 
 #Especificaciones del label
 
+
 hoja_vida = Label(p5Frame, text=next(circulo), font=("Times", 10), fg="black")
 hoja_vida.place(relx=0.5, rely=0.5, anchor=CENTER) #centra el texto
 hoja_vida.bind("<Button-1>", eveto_label)
+
 
 
 def abrir_ventana_principal():
@@ -163,18 +185,23 @@ def abrir_ventana_principal():
     inicializar()
 
 
+
 #p4 abrir nueva pantalla
 boton_ventana_principal = Button(p4Frame, text="Abrir", width=5, height=1,background="lightgray", command=abrir_ventana_principal)
 boton_ventana_principal.place(relx=0.5, rely=0.98, anchor="s" )
+
 
 
 #Menu
 menu_inicio = tk.Menu(frame_inicio)
 ventana.config(menu=menu_inicio, height=30)
 
+
 def salir():
     serializar(hotel)
     sys.exit()
+
+
 
 inicio = tk.Menu(menu_inicio)
 menu_inicio.add_cascade(label="Menú", menu=inicio)
