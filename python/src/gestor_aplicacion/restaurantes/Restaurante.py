07e668fbf7abfc4c233124ec39c23cf51a3c9cac
stjuliod09 <124937364+stjuliod09@users.noreply.github.com>
2023-06-19 19:22:20 -0500

    cambios

--- python/src/gestor_aplicacion/restaurantes/Restaurante.py
+++ python/src/gestor_aplicacion/restaurantes/Restaurante.py
@@ -1,14 +1,18 @@
-from gestor_aplicacion.restaurantes import Menu,Mesa
+from gestor_aplicacion.restaurantes import Mesa
+from enum import Enum
 
-class Restaurante:
-    def __init__(self, nombre, precio, id):
-        self.nombre = nombre
-        self.mesas = []
-        self.precio = precio
-        self.id = id
-        self.menu = Menu()
-        self.inicializar()
+class Restaurante(Enum):
+    
+    RESTAURANTE_MEXICANO = (1, 50000)
+    RESTAURANTE_ITALIANO = (2, 25000)
+    RESTAURANTE_GOURMET = (3, 30000)
+    RESTAURANTE_VEGETARIANO = (4, 15000)
 
+    def __init__(self, idRestaurante, precioRestaurante):
+        self.idRestaurante = idRestaurante
+        self.precioRestaurante = precioRestaurante
+        
+        
     def añadirMesa(self, mesa):
         self.mesas.append(mesa)
 
@@ -18,18 +22,21 @@ class Restaurante:
             s += str(mesa)
         return s
 
+    @staticmethod
+    def buscarPorId(idRestaurante):
+        for restaurante in Restaurante:
+            if int(restaurante.getIdRestaurante()) == int(idRestaurante):
+                return restaurante
+
+        print("no encontrado")
+        return None
+
     def __str__(self):
         return self.nombre + ", precio: " + str(self.precio)
 
     def inicializar(self):
         for i in range(1, 8):
             self.añadirMesa(Mesa(i))
-
-    def getMenu(self):
-        return self.menu
-
-    def setMenu(self, menu):
-        self.menu = menu
 
     # Getters y setters
     def getMesas(self):
@@ -38,20 +45,8 @@ class Restaurante:
     def setMesas(self, mesas):
         self.mesas = mesas
 
-    def getNombre(self):
-        return self.nombre
-
-    def setNombre(self, nombre):
-        self.nombre = nombre
-
-    def getPrecio(self):
-        return self.precio
-
-    def setPrecio(self, precio):
-        self.precio = precio
-
-    def getId(self):
-        return self.id
-
-    def setId(self, id):
-        self.id = id
+    def getPrecioResetaurante(self):
+        return self.precioRestaurante
+    
+    def getIdRestaurante(self):
+        return self.idRestaurante
